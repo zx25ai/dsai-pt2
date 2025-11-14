2c727579e1efe5373aa6ad44c5f431d45bb8de59
Teoh TeikToe <teohteiktoe@gmail.com>
2025-11-14 12:42:18 +0000

    1

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import joblib
 import os
 from groq import Groq
+import sqlite3
+import datetime
 
 from dotenv import load_dotenv
 if os.path.exists('.env'):
@@ -20,6 +22,14 @@ def index():
 
 @app.route("/main",methods=["GET","POST"])
 def main():
+    name = request.form.get("q")
+    t = datetime.datetime.now()
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute('INSERT INTO user (name,timestamp) VALUES(?,?)',(name,t))
+    conn.commit()
+    c.close()
+    conn.close()
     return(render_template("main.html"))
 
 @app.route("/dbs",methods=["GET","POST"])
@@ -52,6 +62,30 @@ def llama_result():
         {"role": "user", "content": q}])
     r = r.choices[0].message.content
     return(render_template("llama_result.html",r=r))
+
+@app.route("/userlog",methods=["GET","POST"])
+def userlog():
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute('''select *
+    from user''')
+    r=""
+    for row in c:
+        print(row)
+        r = r + str(row)
+    c.close()
+    conn.close()
+    return(render_template("userlog.html",r=r))
+
+@app.route("/deletelog",methods=["GET","POST"])
+def deletelog():
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute('DELETE FROM user',);
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("deletelog.html"))
 
 if __name__ == "__main__":
     app.run()
